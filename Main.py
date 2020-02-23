6251ecac3af00b3994797433ac10f6453cf52e7c
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-02-23 06:48:45 -0800

    files chooser

--- Main.py
+++ Main.py
@@ -1,24 +1,37 @@
 #from tkinter.filedialog import askopenfilename
+#from tkinter.filedialog import askdirectory
 #from tkinter import Tk
-from word import BrailleToBangla
+import tkinter
+from tkinter import filedialog
+from oopBraille import BrailleToBangla
+from oopBraille import fileManager
+from os.path import join
 
 #Tk.withdraw()
-fileName = 'G:\\f drive\\1IIT\\5th semester\\SPL2\\test.txt'
-print(fileName)
-file = open(fileName, 'r')
+root = tkinter.Tk()
+filez = filedialog.askopenfilenames(parent=root, title='Choose files')
+fileList = list(filez)
+#folderPath = askdirectory()
+fileManager = fileManager.FileManager(fileList)
+fileList = fileManager.getFiles()
+#fileName = 'G:\\5 th semester\\spl2\\dataFrom Online\\data_jackson.txt'
+for fileName in fileList:
+    print(fileName)
+    file = open(fileName, 'r', )
+    #file = open(fileName, 'r')
 
-lines = file.readlines()
-print(lines)
-text = ''
-brailleToBangla = BrailleToBangla.BrailleToBangla(lines)
-for line in lines:
-    words = line.split(' space ')
-    # print(words)
-    for word in words:
-        letters = word.split(' ')
-        text += brailleToBangla.getBrailleToBangla() #(letters)
-    text += '\n'
+    lines = file.readlines()
+    print(lines)
+    text = ''
+    brailleToBangla = BrailleToBangla.BrailleToBangla()
+    for line in lines:
+        words = line.split(' space ')
+        # print(words)
+        for word in words:
+            letters = word.split(' ')
+            text += brailleToBangla.getBrailleToBangla(letters) #(letters)
+        text += '\n'
 
-print(text)
-file.close()
+    print(text)
+    file.close()
 quit()

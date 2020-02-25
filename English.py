49a222670f4aafd743ed0095f2d7b5b5e8237626
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-02-25 07:02:43 -0800

    Update English.py

--- English.py
+++ English.py
@@ -1,6 +1,9 @@
-from word.CommonSymbols import CommonSymbols
+from oopBraille.CommonSymbols import CommonSymbols
+from collections import defaultdict
 
 class English(CommonSymbols):
+    englishDictionary = defaultdict(list)
+
     english_numbers = {
         '010110': '0', '100000': '1',
         '110000': '2', '100100': '3',
@@ -9,7 +12,7 @@ class English(CommonSymbols):
         '110010': '8', '010100': '9',
     }
 
-    english = {
+    english_alphabet = {
         '100000': 'a', '110000': 'b', '100100': 'c', '100110': 'd', '100010': 'e',
         '110100': 'f', '110110': 'g', '110010': 'h', '010100': 'i', '010110': 'j',
         '101000': 'k', '111000': 'l', '101100': 'm', '101110': 'n', '101010': 'o',
@@ -22,3 +25,16 @@ class English(CommonSymbols):
         CommonSymbols.__init__(self)
         print('english')
         pass
+
+    def getEnglishNumbers(self):
+        return self.english_numbers
+
+    def getEnglishAlphabets(self):
+        return self.english_alphabet
+
+    def getEnglishDictionary(self):
+        for d in (self.english_alphabet, self.punctuation, self.dot):  # you can list as many input dicts as you want here
+            for key, value in d.items():
+                self.englishDictionary[key].append(value)
+
+        return self.englishDictionary

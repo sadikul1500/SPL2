00a802c8e568f314e68da7dc225ccc20047ce19c
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-02-27 07:02:41 -0800

    Update BrailleToEnglish.py

--- BrailleToEnglish.py
+++ BrailleToEnglish.py
@@ -1,18 +1,19 @@
 from oopBraille import English
 from oopBraille import DoubleMap
 
-#Bangla
+#english
 class BrailleToEnglish:
 
     english = English.English()
     numeral_sign = '001111'
+    capital_sign = '000001'
 
     def __init__(self):
         #self.letters = letters
-        print('bangla text process')
+        print('english text process')
 
     def numberProcess(self, letters, bracket_count, i, length):
-        if letters[i] in self.english.operator.keys():
+        if letters[i] in self.english.getOperator().keys():
             if letters[i] == '011011' and bracket_count == 0:
                 bracket_count += 1
                 return '(', bracket_count, i
@@ -29,8 +30,8 @@ class BrailleToEnglish:
             return self.english.getEnglishNumbers().get(letters[i])[0], bracket_count, i
 
         elif (letters[i] not in self.english.getOperator().keys() or letters[
-            i] in self.english.getEnglishNumbers().keys()):
-            if i + 1 < length and letters[i] == '001010' and letters[i + 1] == '001010':
+            i] in self.english.getEnglishNumbers().keys()) and i + 1 < length:
+            if  letters[i] == '001010' and letters[i + 1] == '001010':
                 return self.english.getOperator().get(letters[i] + letters[i + 1])[0], bracket_count, i+1
 
             elif letters[i] == '000011' and letters[i + 1] == '011011':
@@ -43,6 +44,11 @@ class BrailleToEnglish:
             elif letters[i] == '011011' and letters[i + 1] == '000001':
                 return ']', bracket_count, i+1
 
+            else:
+                return '', bracket_count, i
+
+        else:
+            return '', bracket_count, i
 
 
     def getBrailleToText(self, letters):
@@ -50,6 +56,7 @@ class BrailleToEnglish:
 
         length = len(letters)
         num = False
+        capital = False
         i = 0
         text = ''
         #global
@@ -81,51 +88,32 @@ class BrailleToEnglish:
                         text += doubleMap.getCharFromDoubbleMap()
                         bracket_count = doubleMap.getBracket_count()
                         i += 1
-                        '''
-                    elif letters[i] == '000100' and i + 2 < length:
 
-                        text += dd.get(
-                            letters[i + 1])[0] + hos + dd.get(letters[i + 2])[0]
-                        i += 2
-                        # print(2)
-                        # print(hos)
+                    elif letters[i] == self.capital_sign and i+1<length and letters[i+1] == self.capital_sign:
+                        text += self.capital_sign+self.capital_sign
+                        i += 1
 
+                    elif letters[i] == self.capital_sign:
+                        capital = True
 
-
-                    elif letters[i] == '000101' and i + 4 < length:
-                        # joint = ''
-                        joint = dd.get(letters[i + 1])[0] + dd.get(letters[i + 2])[0] + dd.get(letters[i + 3])[0] + \
-                                dd.get(letters[i + 4])[0]
-                        if joint in self.english.getFourLetters().keys():
-                            for key, value in self.bangla.getFourLetters().items():
-                                if joint == key:
-                                    text += dd.get(letters[i + 1])[0] + hos + dd.get(letters[i + 2])[0] + hos + \
-                                            dd.get(letters[i + 3])[0] + hos + dd.get(letters[i + 4])[0]
-                                    i += 4
-                                    # print('4')
-                                    break
-
-                        else:
-                            # joint = dd.get(letters[i + 1]) + dd.get(letters[i + 2]) + dd.get(letters[i + 3])
-
-                            text += dd.get(letters[i + 1])[0] + hos + dd.get(letters[i + 2])[0] + hos + \
-                                    dd.get(letters[i + 3])[0]
-                            i += 3
-
-
-                        '''
                     else:
-
+                        flag = 0
                         for k, v in dd.items():
                             if letters[i] == k:
-                                text += v[0]
-
+                                if capital == True:
+                                    text += v[0].upper()
+                                    capital = False
+                                else:
+                                    text += v[0]
+                                flag = 1
+                                #print('ok')
                                 break
-                            else:
-                                text += k
+                        if flag == 0:
+                            text += letters[i]
 
                     num = False
 
             i += 1
         text += ' '
         return text
+

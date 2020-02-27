d39dcb89800ec930170ebea1fff22509e0b22890
sadikul haque sadi <bsse1003@iit.du.ac.bd>
2020-02-27 07:17:35 -0800

    Update fileManager.py

--- fileManager.py
+++ fileManager.py
@@ -1,13 +1,19 @@
 import os
 from os.path import join
+import tkinter
+from tkinter import filedialog
 
-class FileManager:
-    def __init__(self, fileList):   #folderpath
-        self.fileList = fileList
+class FileManager:  #files = root.tk.splitlist(files)
+    def __init__(self): #folderpath
+        pass
+        #print(self.fileList)
+
 
     def getFiles(self):
         #braille_code = os.listdir(self.fileLocation)
-        final_list = self.fileList[:]
+        root = tkinter.Tk()
+        fileList = root.tk.splitlist(filedialog.askopenfilenames(parent=root, title='Choose files'))
+        final_list = fileList[:]
         for iFile in final_list:
 
             if iFile.lower().endswith(('.txt', '.doc', '.docx')):
